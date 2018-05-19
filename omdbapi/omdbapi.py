9092c7aa3886e5a52cc884fd9cb28ec2f53b224c
Diego Ubirajara <diego.ubirajara@mail.com>
2018-05-19 09:36:05 +0200

    refactor omdbapi

--- omdbapi/omdbapi.py
+++ omdbapi/omdbapi.py
@@ -3,17 +3,50 @@ import requests
 
 
 class MovieSearch:
-    def __init__(self, api_key):
-        self.api_key = api_key
+    def __init__(self, api_key, title, plot=None):
+        """
+        instantiate the class, passing api key and title as parameter.
 
-    def get_movie(self, title, plot=None):
-        self.url = f'http://www.omdbapi.com/?t={title}&y=&plot={plot}&r=json&apikey={self.api_key}'
+        :param api_key: ombdapi key
+        :param title: title movie
+        :param plot: optional, description plot, default return short plot or set full.
+
+        :Example:
+        m = MovieSearch('your api key', 'title movie')
+        :Example:
+        m = MovieSearch('your api key', 'title movie' 'full')
+        """
+        self.title = title
+        self.api_key = api_key
+        self.url = f'http://www.omdbapi.com/?t={self.title}&y=&plot={plot}&r=json&apikey={self.api_key}'
         self.response = requests.get(self.url)
         self.values = json.loads(self.response.text)
 
-    def all(self):
-        for k, v in self.values.items():
-            print(k, ':', v)
+    def __repr__(self):
+        return f"MovieSearch('{self.api_key}', '{self.title}')"
 
-    def get(self, item):
-        return self.values[item.title()]
+    def get_all_data(self):
+        """
+        Get all data movie, key: value
+
+        :Example:
+        m.get_all_data()
+        """
+        if self.values['Response'] == 'True':
+            return self.values
+        else:
+            print(self.values['Error'])
+
+    def get_data(self, *args):
+        """
+        Get values passing keys as parameter.
+
+        :param *args: items data key
+
+        :Example:
+        m.get_data('Director', 'Actors')
+        """
+        items = {}
+        for item in args:
+            items[item] = self.values[item.title()]
+        return items

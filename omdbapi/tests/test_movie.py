19fa3c241f700bad6e22fcbd757b5d851ffac93a
Diego Ubirajara <diego.ubirajara@mail.com>
2018-06-10 17:22:06 +0200

    add test

--- omdbapi/tests/test_movie.py
+++ omdbapi/tests/test_movie.py
@@ -1,2 +1,20 @@
-def test_int():
-    assert 1 == 1
+from unittest.mock import Mock
+
+from omdbapi import movie_search
+
+
+def test_get_movie():
+    resp_mock = Mock()
+    resp_mock.json.return_value = {
+        'Title': 'Star Wars: Episode IV - A New Hope',
+        'Year': '1977',
+        'Genre': 'Action, Adventure, Fantasy',
+        'Director': 'George Lucas',
+        'Writer': 'George Lucas',
+        'Actors': 'Mark Hamill, Harrison Ford, Carrie Fisher, Peter Cushing',
+        'Awards': 'Won 6 Oscars. Another 50 wins & 28 nominations.',
+        'Response': 'True'
+         }
+    movie_search.requests.get = Mock(return_value=resp_mock)
+    url = movie_search.GetMovie('12345', 'star wars').get_all_data()
+    assert url['Response'] == 'True'

303a3df15ff2bd651e2352a44e860387a0a8c2c8
Diego Ubirajara <diego.ubirajara@mail.com>
2018-06-11 19:10:05 +0200

    improve more tests

--- omdbapi/tests/test_movie.py
+++ omdbapi/tests/test_movie.py
@@ -1,9 +1,15 @@
 from unittest.mock import Mock
 
+import pytest
+
 from omdbapi import movie_search
 
 
-def test_get_movie():
+@pytest.mark.parametrize(
+    'movie',
+    ['Title', 'Awards', 'Year', 'Genre', 'Writer', 'Response', 'Actors', 'Director']
+)
+def test_get_movie(movie):
     resp_mock = Mock()
     resp_mock.json.return_value = {
         'Title': 'Star Wars: Episode IV - A New Hope',
@@ -17,4 +23,18 @@ def test_get_movie():
          }
     movie_search.requests.get = Mock(return_value=resp_mock)
     url = movie_search.GetMovie('12345', 'star wars').get_all_data()
-    assert url['Response'] == 'True'
+    # assert url['Response'] == 'True'
+    assert movie in url
+
+
+def test_repr():
+    assert repr(movie_search.GetMovie('12345', 'star wars')) == "GetMovie('12345', 'star wars')"
+
+
+@pytest.mark.parametrize(
+    'movie',
+    ['Title', 'Awards', 'Year']
+)
+def test_get_data(movie):
+    url = movie_search.GetMovie('12345', 'star wars').get_data('Title', 'Awards', 'Year')
+    assert movie in url
